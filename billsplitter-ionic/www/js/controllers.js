4b8e54671971ed38b2a9b6a5253d98cb7a7a1537
Bojana Dumeljic <bojana.dumeljic@gmail.com>
2015-11-28 15:02:06 +0100

    Ionic web app
    Dashboard, see bills, bill details and add bill

--- billsplitter-ionic/www/js/controllers.js
+++ billsplitter-ionic/www/js/controllers.js
@@ -1,28 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('DashCtrl', function($scope, Bills) {
+  $scope.participants = Bills.allParticipants();
 })
 
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
   };
+})
+
+.controller('BillsCtrl', function($scope, Bills) {
+  $scope.bills = Bills.all();
+  $scope.remove = function(bill) {
+    Bills.remove(bill);
+  }
+})
+
+.controller('BillDetailCtrl', function($scope, $stateParams, Bills) {
+  $scope.bill = Bills.get($stateParams.billId);
+})
+
+.controller('AddBillCtrl', function($scope) {
+
 });
--- billsplitter-ionic/www/js/services.js
+++ billsplitter-ionic/www/js/services.js
@@ -1,50 +1,63 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+.factory('Bills', function() {
+    var bills = [{
+        id: 0,
+        payee: 'Ben Sparrow',
+        amount: '$ 5,20',
+        description: 'Coffee at Wayne\'s',
+        face: 'img/ben.png'
+    }, {
+        id: 1,
+        payee: 'Mike Harrington',
+        amount: '$ 15,40',
+        description: 'Dinner at Vigarda',
+        face: 'img/mike.png'
+    }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    var participants = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        face: 'img/ben.png',
+        balance: -27
+    }, {
+        id: 1,
+        name: 'Mike Harrington',
+        face: 'img/mike.png',
+        balance: -53
+    }, {
+        id: 2,
+        name: 'Adam Doe',
+        face: 'img/adam.jpg',
+        balance: 80
+    }];
+
+    return {
+        all: function() {
+            return bills;
+        },
+        allParticipants: function() {
+          return participants;
+        },
+        remove: function(bill) {
+            bills.splice(bills.indexOf(bill), 1);
+        },
+        get: function(billId) {
+            for (var i = 0; i < bills.length; i++) {
+                if (bills[i].id === parseInt(billId)) {
+                    return bills[i];
+                }
+            }
+            return null;
+        },
+        addBill: function(bill) {
+            bills.push({
+                payee: 'Adam Doe',
+                amount: bill.amount,
+                description: bill.description,
+                face: 'img/adam.png'
+            })
         }
-      }
-      return null;
     }
-  };
 });
