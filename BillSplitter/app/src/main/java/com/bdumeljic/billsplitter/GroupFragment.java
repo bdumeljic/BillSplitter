4a88a419c5f18cd8227e84c81eea91e03afc7c69
Bojana Dumeljic <bojana.dumeljic@gmail.com>
2015-12-08 17:28:33 +0100

    fix layout

--- BillSplitter/app/src/main/java/com/bdumeljic/billsplitter/GroupFragment.java
+++ BillSplitter/app/src/main/java/com/bdumeljic/billsplitter/GroupFragment.java
@@ -81,6 +81,8 @@ public class GroupFragment extends Fragment {
         return fragment;
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
